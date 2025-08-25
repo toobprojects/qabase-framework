b5c801237969e7337eda9fc19ad83f08cd99105f
Thabo Matjuda <tl.matjuda@gmail.com>
2025-08-25 13:39:37 +0200

    test(rest): CLearned up the configs namespace for rest apis.

--- qabase-rest/src/main/kotlin/com/toob/qabase/http/client/RestModuleConstants.kt
+++ qabase-rest/src/main/kotlin/com/toob/qabase/http/client/RestModuleConstants.kt
@@ -2,7 +2,7 @@ package com.toob.qabase.http.client
 
 
 object RestModuleConstants {
-    const val REST_SERVICE = "rest-service"
+    const val REST_SERVICE = "qabase.rest"
 
     const val AUTHORIZATION = "Authorization"
     const val BEARER = "Bearer"
--- qabase-rest/src/test/kotlin/com/toob/qabase/http/RestModuleConfigsIT.kt
+++ qabase-rest/src/test/kotlin/com/toob/qabase/http/RestModuleConfigsIT.kt
@@ -1,6 +1,7 @@
 package com.toob.qabase.http
 
 import com.toob.qabase.QaBaseTest
+import com.toob.qabase.util.logger
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
@@ -9,13 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired
 @QaBaseTest
 class RestModuleConfigsIT {
 
+	private val log = logger()
+
     @Autowired
     lateinit var restModuleConfigs: RestModuleConfigs
 
     @Test
-    fun `Should Load Confiurations`() {
+    fun `Should load configurations`() {
         assertNotNull(restModuleConfigs)
         assertNotNull( restModuleConfigs.baseUrl)
         assertEquals("https://jsonplaceholder.typicode.com", restModuleConfigs.baseUrl)
+		log.info { "✅ Rest configs loaded successfully" }
     }
 }
